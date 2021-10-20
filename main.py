45a52f40dfcc5fa40106b666de94934b2dc92fc5
TheCaptainCraken <thecaptaincraken@gmail.com>
2021-10-20 18:35:17 +0200

    added comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from bfi import *
 
+#try open a file
 try:
     with open(sys.argv[1], 'r') as f:
         tha_code = f.read()
@@ -8,6 +9,6 @@ except FileNotFoundError:
     print('Use a valid path not "{0}"! >:/'.format(sys.argv[1]))
     sys.exit()
 
-
+#create an interpreter to then execute the code
 interpreter = Bfi(tha_code)
 interpreter.execute()
